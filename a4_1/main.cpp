c4a7b8671526ff3b530ebc307e8ae6558a2bceca
nyccowgirl <nyc_cowgirl@hotmail.com>
2020-09-24 00:18:53 -0500

    Initial Commit

---
+++ a4_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  a4_1
+//
+//  Created by nyccowgirl on 9/24/20.
+//  Copyright © 2020 nyccowgirl. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
